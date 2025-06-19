10cd9e44ffd5d322fc61689af26d2ee3f42e1d7b
Josue Perez <kokikam@gmail.com>
2025-06-19 14:54:35 -0600

    254 JC

--- simple_controller-Entrenado.py
+++ simple_controller-Entrenado.py
@@ -106,7 +106,8 @@ def main():
     while robot.step() != -1:
         # Get image from camera
         image = get_image(camera)
-        image = image[110:135,:,:]
+        image = image[110:150,130:210:]
+        #image = image[110:150,:,:]
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         #image = cv2.GaussianBlur(image, (3, 3), 0)
         image = cv2.resize(image, (200, 66))
